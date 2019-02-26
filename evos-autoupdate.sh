db5d1dd3d045a3c62fdcdf67409b5dc989e63bf2
RasmonT <35234558+RasmonT@users.noreply.github.com>
2019-02-26 05:39:02 +0100

    Update evos-autoupdate.sh

--- evos-autoupdate.sh
+++ evos-autoupdate.sh
@@ -7,6 +7,6 @@ NC='\033[0m'
 
 echo -e "${GREEN} Preparing actual script to start. ${NC}"
 sleep 1s
-wget https://raw.githubusercontent.com/RasmonT/TestFiles/master/esbc-updatepro.sh
+wget https://raw.githubusercontent.com/RasmonT/TestFiles/master/evos-updatepro.sh
 sleep 2s
 bash esbc-updatepro.sh
